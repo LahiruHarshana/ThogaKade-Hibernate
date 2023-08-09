f820252277a9e98e7312fe5c640f1b7472f5229a
lahiruharshana <lharshana2002@>
2023-08-09 09:56:33 +0530

    0.1.3

---
+++ src/lk/ijse/thogakade/hibernate/repository/CustomerRepository.java
@@ -0,0 +1,65 @@
+package lk.ijse.thogakade.hibernate.repository;
+
+import lk.ijse.thogakade.hibernate.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class CustomerRepository {
+    private final Session session;
+    public CustomerRepository(){
+        session = SessionFactoryConfig.getInstance().getSession();
+
+    }
+    public Customer getCustomer(int id){
+        try {
+            return session.get(Customer.class,id);
+        }catch (Exception e){
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public int saveCustomer(Customer customer){
+        Transaction transaction = session.beginTransaction();
+        try {
+            int customerId = (int) session.save(customer);
+            transaction.commit();
+            session.close();
+            return customerId;
+        }catch (Exception e){
+            transaction.rollback();
+            session.close();
+            e.printStackTrace();
+            return -1;
+        }
+    }
+    public boolean updateCustomer(Customer customer){
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.update(customer);
+            return true;
+        }catch (Exception e){
+            transaction.rollback();
+            session.close();
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public boolean deleteCustomer(Customer customer){
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.delete(customer);
+            transaction.commit();
+            session.close();
+            return true;
+
+        }catch (Exception e){
+            transaction.rollback();
+            session.close();
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
