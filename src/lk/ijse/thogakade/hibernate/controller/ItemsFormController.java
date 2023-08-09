24274a03b2225848e82ca3fb97a74b2b3caeefa4
lahiruharshana <lharshana2002@>
2023-08-09 09:36:04 +0530

    0.0.8

--- src/lk/ijse/thogakade/hibernate/controller/ItemsFormController.java
+++ src/lk/ijse/thogakade/hibernate/controller/ItemsFormController.java
@@ -1,4 +1,79 @@
 package lk.ijse.thogakade.hibernate.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
 public class ItemsFormController {
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXButton btnSave;
+
+    @FXML
+    private JFXButton btnSearch;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colCode;
+
+    @FXML
+    private TableColumn<?, ?> colDes;
+
+    @FXML
+    private TableColumn<?, ?> colPrice;
+
+    @FXML
+    private TableColumn<?, ?> colQty;
+
+    @FXML
+    private ImageView imgHome;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private TableView<?> tblItems;
+
+    @FXML
+    private TextField txtItemCode;
+
+    @FXML
+    private TextField txtItemDes;
+
+    @FXML
+    private TextField txtItemUnit;
+
+    @FXML
+    private TextField txtQty;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event){
+
+    }
+
 }
