c9fe09ee8f6c853e1aea7d660dd16a46eb8ec4ac
lahiruharshana <lharshana2002@>
2023-08-09 09:50:10 +0530

    0.1.1

--- src/lk/ijse/thogakade/hibernate/entity/Customer.java
+++ src/lk/ijse/thogakade/hibernate/entity/Customer.java
@@ -11,7 +11,41 @@ public class Customer {
     private String id;
     @Column(name = "Name")
     private String name;
+
+
     @Column(name = "Address")
     private String address;
 
+    public Customer() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Customer(String id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
 }
