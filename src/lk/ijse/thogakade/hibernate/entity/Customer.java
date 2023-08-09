8e6da264e4884c18cfc43301be60fe06484a9adc
lahiruharshana <lharshana2002@>
2023-08-09 09:48:46 +0530

    0.2.0

--- src/lk/ijse/thogakade/hibernate/entity/Customer.java
+++ src/lk/ijse/thogakade/hibernate/entity/Customer.java
@@ -9,7 +9,9 @@ public class Customer {
     @Id
     @Column(name = "ID")
     private String id;
+    @Column(name = "Name")
     private String name;
+    @Column(name = "Address")
     private String address;
 
 }
