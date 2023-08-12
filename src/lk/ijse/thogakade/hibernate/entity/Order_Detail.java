f00876dd648c3ef291efee412cd583454719c1f8
lahiruharshana <lharshana2002@>
2023-08-12 16:10:02 +0530

    0.4.3

--- src/lk/ijse/thogakade/hibernate/entity/Order_Detail.java
+++ src/lk/ijse/thogakade/hibernate/entity/Order_Detail.java
@@ -1,4 +1,10 @@
 package lk.ijse.thogakade.hibernate.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "oder_details")
 public class Order_Detail {
+
 }
