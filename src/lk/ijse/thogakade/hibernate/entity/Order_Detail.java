9f89fc966ca35a49e8f0990fb861e86412053d9c
lahiruharshana <lharshana2002@>
2023-08-12 16:12:10 +0530

    0.4.4

--- src/lk/ijse/thogakade/hibernate/entity/Order_Detail.java
+++ src/lk/ijse/thogakade/hibernate/entity/Order_Detail.java
@@ -1,10 +1,16 @@
 package lk.ijse.thogakade.hibernate.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "oder_details")
 public class Order_Detail {
+    @Column(name = "order_quantity")
+    private int qty;
+    @Column(name = "order_price")
+    private double price;
+
 
 }
