87bd27464f759dfb2a686c53cae0a81702469384
lahiruharshana <lharshana2002@>
2023-08-12 16:21:17 +0530

    0.5.0

--- src/lk/ijse/thogakade/hibernate/embeded/OrderDetailPK.java
+++ src/lk/ijse/thogakade/hibernate/embeded/OrderDetailPK.java
@@ -2,9 +2,12 @@ package lk.ijse.thogakade.hibernate.embeded;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
 
 @Embeddable
 public class OrderDetailPK {
+    @EmbeddedId
+    private OrderDetailPK orderDetailPK;
     @Column(name = "order_id")
     private int orderId;
     @Column(name = "item_id")
