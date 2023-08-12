f104af8cd70acaf44ce68a038c45d9cd9dfadfe9
lahiruharshana <lharshana2002@>
2023-08-12 13:53:14 +0530

    0.2.5

--- src/lk/ijse/thogakade/hibernate/entity/Customer.java
+++ src/lk/ijse/thogakade/hibernate/entity/Customer.java
@@ -1,5 +1,60 @@
 package lk.ijse.thogakade.hibernate.entity;
 
-public class Customer {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Customer")
+public class Customer {
+    @Id
+    @Column(name = "customer_id")
+    private String id;
+    @Column(name = "customer_name")
+    private String name;
+    @Column(name = "customer_address")
+    private String address;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Customer() {
+    }
+
+    public Customer(String id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
