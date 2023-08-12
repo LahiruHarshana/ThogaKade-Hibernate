584d660eaa461303cfb6452955a400055a594032
lahiruharshana <lharshana2002@>
2023-08-12 16:19:33 +0530

    0.4.9

--- src/lk/ijse/thogakade/hibernate/embeded/OrderDetailPK.java
+++ src/lk/ijse/thogakade/hibernate/embeded/OrderDetailPK.java
@@ -10,6 +10,35 @@ public class OrderDetailPK {
     @Column(name = "item_id")
     private int itemId;
 
+    public OrderDetailPK() {
+    }
 
+    public OrderDetailPK(int orderId, int itemId) {
+        this.orderId = orderId;
+        this.itemId = itemId;
+    }
 
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetailPK{" +
+                "orderId=" + orderId +
+                ", itemId=" + itemId +
+                '}';
+    }
 }
