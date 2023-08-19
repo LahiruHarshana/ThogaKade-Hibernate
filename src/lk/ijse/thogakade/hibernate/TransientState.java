aab63ffdbac9f7d53addbd73950c994141642944
lahiruharshana <lharshana2002@>
2023-08-19 10:59:49 +0530

    0.6.0

--- src/lk/ijse/thogakade/hibernate/TransientState.java
+++ src/lk/ijse/thogakade/hibernate/TransientState.java
@@ -1,7 +1,14 @@
 package lk.ijse.thogakade.hibernate;
 
+import lk.ijse.thogakade.hibernate.entity.Customer;
+
 public class TransientState {
     public static void main(String[] args) {
+        //Transient
+        Customer customer = new Customer();
+        customer.setName("Saman");
+        customer.setAddress("Galle");
 
     }
+
 }
