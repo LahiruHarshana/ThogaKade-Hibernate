b9b240125ae710d48c0e293169c8f5406fdc22e5
lahiruharshana <lharshana2002@>
2023-08-19 11:04:52 +0530

    0.6.1

--- src/lk/ijse/thogakade/hibernate/TransientState.java
+++ src/lk/ijse/thogakade/hibernate/TransientState.java
@@ -1,14 +1,24 @@
 package lk.ijse.thogakade.hibernate;
 
+import lk.ijse.thogakade.hibernate.config.SessionFactoryConfig;
 import lk.ijse.thogakade.hibernate.entity.Customer;
+import org.hibernate.Session;
 
 public class TransientState {
     public static void main(String[] args) {
+
+        Session session = SessionFactoryConfig.getInstance().getSession();
         //Transient
         Customer customer = new Customer();
         customer.setName("Saman");
         customer.setAddress("Galle");
 
+        System.out.println(session.contains(customer));
+
+    }
+
+    private String isExistsInSession(Session session ,Customer customer){
+       return session.contains(customer)?"Customer mapped with a session " : "Customer not mapped with session";
     }
 
 }
