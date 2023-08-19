3cfe11d3cb3c2c40eddf4d01ffc8193da2b1efcd
lahiruharshana <lharshana2002@>
2023-08-19 11:17:35 +0530

    0.6.3

--- src/lk/ijse/thogakade/hibernate/PersistentState.java
+++ src/lk/ijse/thogakade/hibernate/PersistentState.java
@@ -1,4 +1,17 @@
 package lk.ijse.thogakade.hibernate;
 
+import lk.ijse.thogakade.hibernate.config.SessionFactoryConfig;
+import lk.ijse.thogakade.hibernate.entity.Customer;
+import org.hibernate.Session;
+
 public class PersistentState {
+    public static void main(String[] args) {
+        Session session = SessionFactoryConfig.getInstance().getSession();
+        //Transient
+        Customer customer = new Customer();
+        customer.setName("Saman");
+        customer.setAddress("Galle");
+
+        System.out.println(session.contains(customer));
+    }
 }
