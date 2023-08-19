1776faa6bb1d56e2882dc4a0bde7e2d1039fce9f
lahiruharshana <lharshana2002@>
2023-08-19 10:15:07 +0530

    0.5.5

--- src/lk/ijse/thogakade/hibernate/GetVsLoad.java
+++ src/lk/ijse/thogakade/hibernate/GetVsLoad.java
@@ -1,5 +1,21 @@
 package lk.ijse.thogakade.hibernate;
 
-public class GetVsLoad {
+import lk.ijse.thogakade.hibernate.config.SessionFactoryConfig;
+import lk.ijse.thogakade.hibernate.entity.Customer;
+import org.hibernate.Session;
 
+public class GetVsLoad {
+    public static void main(String[] args) {
+        Session session = SessionFactoryConfig
+                .getInstance().getSession();
+        int cusId = (int) session.save(getCustomer());
+        session.close();
+    }
+
+    private static Customer getCustomer() {
+        Customer customer = new Customer();
+        customer.setName("Saman");
+        customer.setAddress("Galle");
+        return customer;
+    }
 }
